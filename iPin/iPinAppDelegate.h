e971071734645ea865748f65119cef6b86c9aad1
MyAngelLYC <ycliu0930@gmail.com>
2014-06-11 20:11:54 +0800

    Initial Commit

---
+++ iPin/iPinAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  iPinAppDelegate.h
+//  iPin
+//
+//  Created by 刘亦辰 on 14-6-11.
+//  Copyright (c) 2014年 ___iPinStudio___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iPinAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ iPin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iPin
+//
+//  Created by 刘亦辰 on 14-6-11.
+//  Copyright (c) 2014年 ___iPinStudio___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iPinAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([iPinAppDelegate class]));
+    }
+}
